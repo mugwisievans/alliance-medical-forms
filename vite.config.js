70d99b1f843ebcc3c1ca25fe7caacf4397283afa
mugwisievans <40376369+mugwisievans@users.noreply.github.com>
2025-12-01 14:16:25 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import htmlBundle from "rollup-plugin-html-bundle";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      ...htmlBundle({
-        template: "index.html",
-        dest: "dist",
-        inline: true,       // inline ALL JS + CSS + images
-        minify: true
-      }),
-      enforce: "post",
-      apply: "build",
-    },
-  ],
-
-  build: {
-    target: "esnext",
-    cssCodeSplit: false,
-    assetsInlineLimit: Infinity,
-    modulePreload: false,
-
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-
-  base: "./",
-  
-});
-
+  base: "/alliance-medical-forms/",   // REQUIRED FOR GitHub Pages
+  plugins: [react()],
+})
